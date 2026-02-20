f77b0728a150d42f49f1a1d9fccd8d2f2912eff7
GabsCavalcant <gabrielnina22@gmail.com>
2026-02-19 22:16:39 -0300

    Creation of project

---
+++ src/main/java/com/gabrielCant/ecommerce_mongo/EcommerceMongoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielCant.ecommerce_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielCant/ecommerce_mongo/EcommerceMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielCant.ecommerce_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
