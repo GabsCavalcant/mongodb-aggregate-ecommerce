39a5f28717626a847bfa71eedfaeeff535b7bb02
GabsCavalcant <gabrielnina22@gmail.com>
2026-02-23 21:31:07 -0300

    Obtain post for ID

---
+++ src/main/java/com/gabrielCant/ecommerce_mongo/resource/PostResource.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gabrielCant.ecommerce_mongo.resource;
+
+import com.gabrielCant.ecommerce_mongo.domain.Post;
+import com.gabrielCant.ecommerce_mongo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author gabri
+ */
+//ResponseEntity para retornar uma sinal http válido
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+   
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+
+        Post user = service.findById(id);
+
+        return ResponseEntity.ok().body(user);
+    }
+    
+    
+
+}
---
+++ src/main/java/com/gabrielCant/ecommerce_mongo/service/PostService.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gabrielCant.ecommerce_mongo.service;
+
+import com.gabrielCant.ecommerce_mongo.domain.Post;
+import com.gabrielCant.ecommerce_mongo.repository.PostRepository;
+import com.gabrielCant.ecommerce_mongo.service.exception.ObjectNotFound;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author gabri
+ */
+@Service
+public class PostService {
+    
+    @Autowired 
+    private PostRepository repo;
+    
+    public Post findById(String id){
+        Optional<Post> obj = repo.findById(id);
+        
+        return obj.orElseThrow(() -> new ObjectNotFound("Objeto Não encontrado!"));
+    }
+    
+}
